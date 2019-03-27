4f984b6912d8d9addfff9489864e597194ffd37a
Jeremy <jrmsweitzer712@yahoo.com>
2019-03-27 08:12:47 -0400

    Cleaned up Wild Tornado Added 5 cards from SBLS

---
+++ CardShuffler/Models/Yugioh/YugiohCards/Monsters/SphereKuriboh.cs
@@ -0,0 +1,19 @@
+﻿using CardShuffler.Models.Yugioh.YugiohCardTypes;
+
+namespace CardShuffler.Models.Yugioh.YugiohCards
+{
+    public class SphereKuriboh : EffectMonster
+    {
+        public SphereKuriboh(YugiohGame game) : base(game)
+        {
+            Name = "Sphere kuriboh";
+            Attribute = MonsterAttribute.Dark;
+            Level = 1;
+            Type = MonsterType.Fiend;
+            ATK = 300;
+            DEF = 200;
+            SetCodes.Add("SBLS-EN018");
+            CardCode = 33245030;
+        }
+    }
+}
---
+++ CardShuffler/Models/Yugioh/YugiohCards/Traps/Michizure.cs
@@ -0,0 +1,14 @@
+﻿using CardShuffler.Models.Yugioh.YugiohCardTypes;
+
+namespace CardShuffler.Models.Yugioh.YugiohCards.Traps
+{
+    public class Michizure : NormalTrap
+    {
+        public Michizure(YugiohGame game) : base(game)
+        {
+            Name = "Michizure";
+            SetCodes.Add("SBLS-EN044");
+            CardCode = 37580756;
+        }
+    }
+}
