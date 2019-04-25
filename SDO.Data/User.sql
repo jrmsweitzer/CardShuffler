bde6cbdf62618f0c47eaf541d31f7a79d8112d02
Jeremy <jrmsweitzer712@yahoo.com>
2019-04-25 07:46:04 -0400

    added forums

---
+++ SDO.Data/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId]			INT	IDENTITY (1, 1) NOT NULL,
+	[Username]			NVARCHAR (50)		NOT NULL,
+	[Email]				NVARCHAR (50)		NOT NULL,
+	[RegistrationDate]	DATETIME			NOT NULL,
+	[LastLoginDate]		DATETIME			NOT NULL,
+	[RoleId]			INT					NOT NULL
+	PRIMARY KEY CLUSTERED ([UserId] ASC),
+	CONSTRAINT [FK_dbo.User_dbo.Role] FOREIGN KEY ([RoleId])
+		REFERENCES [dbo].[Role] ([RoleId]) ON DELETE NO ACTION
+)
